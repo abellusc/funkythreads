9aec50bd7957df3232637dd98f221286bdbc98bd
Alexandra Belluscio <alexandra.belluscio@icloud.com>
2019-10-31 09:15:58 -0400

    fix: add stdio pipes

--- src/funkythread.ts
+++ src/funkythread.ts
@@ -8,9 +8,14 @@ import { Worker } from 'worker_threads';
 export async function runFunctionAsThread(fn: (...params: any) => any, args?: any[]) {
   return await new Promise((resolve, reject) => {
     if ((fn as any).then) { throw new TypeError('Async functions are not allowed.'); }
-    const worker = new Worker(require.resolve('./runTask'), { workerData: { fn: fn.toString(), args } });
+    const worker = new Worker(require.resolve('./runTask'), {
+        workerData: { fn: fn.toString(), args },
+    });
+    worker.stdout.pipe(process.stdout);
+    worker.stderr.pipe(process.stderr);
+
     worker.on('message', (result) => (
-      resolve(new Function(`'use strict'; return (${result})`)()())),
+      resolve(new Function(`'use strict'; return (${result})`)()())), // dynamic code execution is dangerous
     ); // return value is boxed in a function
     worker.on('error', reject);
   });
